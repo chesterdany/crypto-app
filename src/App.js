ec1f0618528c2398fd1a619e77fbfd86c815971b
Brendea Daniel <chesterdany22@yahoo.com>
2022-01-10 15:27:44 +0200

    react router installed, components and images folder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">Navbar</div>
+      <div className="main">Main</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
