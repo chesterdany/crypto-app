8ab98e7b4fa81680a83ed5de64a90aad95a955d4
Brendea Daniel <chesterdany22@yahoo.com>
2022-01-12 19:04:32 +0200

    homepage frame added

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
 const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+  const { data, isFetching } = useGetCryptosQuery();
+  const [coins, setCoins] = useState(data?.data?.coins);
+
+  if (isFetching) return "Loading...";
+  console.log(coins);
+  return <div className=""></div>;
 };
 
 export default Cryptocurrencies;
